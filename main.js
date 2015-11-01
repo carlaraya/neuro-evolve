340f325ae765f3d5187ea2e62e4176446a12db1d
carlaraya <carl_angelo.araya@upd.edu.ph>
2015-11-01 11:42:35 +0800

    Added option to switch between 0-player and 1-player using clickable css button

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-var aiOn = true;
+pipes = 0;
+bird = 0;
+ground = 0;
+population = [];
+
+spacePressed = false;
+
 var canvas = document.getElementById('mainCanvas');
 var ctx = canvas.getContext('2d');
 var pipeSpacing = 300;
 var score = 0.0;
 var groundHeight = 50;
+
+var playerHighScore = 0;
+
+var popSize = 20;
+var generation = 0;
+var creature = 0;
+
+function toggle_mode() {
+    aiOn = !aiOn;
+    init_button_text();
+    init();
+}
+
+function init_button_text() {
+    if (aiOn) {
+        document.getElementById("toggle-mode").innerHTML = "Switch to 1-player";
+    } else {
+        document.getElementById("toggle-mode").innerHTML = "Switch to 0-player";
+    }
+}
 
 function is_collide(bird, pipe) {
     return bird.X_POS + bird.RADIUS >= pipe.xPos
@@ -12,15 +38,26 @@ function is_collide(bird, pipe) {
         || bird.yPos - bird.RADIUS <= pipe.GAP_POS);
 }
 
-function draw_score() {
+function draw_stats() {
     ctx.font = "48px serif";
     ctx.fillStyle = "#FF0000";
     ctx.fillText(score.toFixed(1).toString(), 10, 50);
+    ctx.font = "18px arial";
+    ctx.fillStyle = "#000000";
+    if (aiOn) {
+        ctx.fillText("Generation " + generation.toString(), 10, 70);
+        ctx.fillText("Creature " + creature.toString(), 10, 90);
+    } else {
+        ctx.fillText("Highscore " + playerHighScore.toFixed(1).toString(), 10, 70);
+    }
 }
 
 
 function game_over() {
-    alert("DEAD: Score = " + score.toFixed(1).toString());
+    alert("DEAD: Score = " + score.toFixed(1).toString() + " (Press space or enter)");
+    if (score > playerHighScore) {
+        playerHighScore = score;
+    }
     init();
 }
 
@@ -28,32 +65,68 @@ function init() {
     pipes = [];
     pipes.push(new Pipe(ctx, canvas.width, canvas.height));
     bird = new Bird(ctx, canvas.width, canvas.height);
-    ground = new Ground(ctx, canvas.width, canvas.height, groundHeight);
     score = 0;
+
+    if (population.length == 0) {
+        init_simulation();
+    }
 }
 
-var pipes;
-var bird;
-var ground;
-var neuralnet = new NeuralNet(8, 1, 0, 0);
+function move_on_to_next() {
+        creature++;
+        if (creature == popSize) {
+            creature = 0;
+            generation++;
 
-init();
+            population = [];
+            for (var i = 0; i < popSize; i++) {
+                population.push(new NeuralNet(8, 1, 0, 8));
+            }
+            
+
+        }
+} 
+
+function init_new() {
+    init();
+    init_button_text();
+    ground = new Ground(ctx, canvas.width, canvas.height, groundHeight);
+    if (aiOn) {
+        init_simulation();
+    }
+}
+
+function init_simulation() {
+    creature = 0;
+    for (var i = 0; i < popSize; i++) {
+        population.push(new NeuralNet(8, 1, 0, 8));
+    }
+}
+
+
+init_new();
 
 canvas.addEventListener('click', function() {
     if (!aiOn) {
         bird.flap();
     }
 }, false);
+
 window.addEventListener('keydown', function(e) {
     if (!aiOn) {
         if (e.keyCode == 74) {
             bird.flap();
         }
+        if (e.keyCode == 32) {
+            spacePressed = true;
+        }
     }
+
 }, true);
 window.addEventListener('keyup', function(e) {
     if (!aiOn) {
         bird.release();
+        spacePressed = false;
     }
 }, true);
 function loop() {
@@ -61,24 +134,24 @@ function loop() {
     // ai_stuff
     if (aiOn) {
         var inputs = [];
-        inputs.push(bird.yPos / canvas.height);
+        inputs.push(bird.yPos / canvas.height * 2 - 1);
         inputs.push(bird.yVel / 50);
         var i;
-        for (i = 0; pipes[i].xPos < bird.X_POS; i++);
+        for (i = 0; pipes[i].xPos + pipes[i].WIDTH < bird.X_POS; i++);
 
         var first = i;
         while (i - first < 3 &&  i < pipes.length) {
-            inputs.push(pipes[i].xPos / canvas.width);
-            inputs.push(pipes[i].GAP_POS / canvas.height);
+            inputs.push(pipes[i].xPos / canvas.width * 2 - 1);
+            inputs.push(pipes[i].GAP_POS / canvas.height * 2 - 1);
             i++;
         }
         if (i - first < 3) {
             for (; i - first < 3; i++) {
-                inputs.push(0);
-                inputs.push(0);
+                inputs.push(-1);
+                inputs.push(-1);
             }
         }
-        var outputs = neuralnet.update(inputs);
+        var outputs = population[creature].update(inputs);
         console.log(outputs);
         if (outputs[0] > 0.5) {
             bird.flap();
@@ -89,11 +162,21 @@ function loop() {
     // others
     for (var i = 0; i < pipes.length; i++) {
         if (is_collide(bird, pipes[i])) {
-            game_over();
+            if (aiOn) {
+                init();
+                move_on_to_next();
+            } else {
+                game_over();
+            }
         }
     }
     if (bird.yPos + bird.RADIUS >= canvas.height - ground.HEIGHT) {
-        game_over();
+        if (aiOn) {
+            init();
+            move_on_to_next();
+        } else {
+            game_over();
+        }
     }
 
     score += 0.1;
@@ -117,7 +200,7 @@ function loop() {
     }
     bird.draw();
     ground.draw();
-    draw_score();
+    draw_stats();
 }
 setInterval(loop, 20);
 
